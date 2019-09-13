050dc6b9ee8f276aed5dd5afaf41ca385ca6ced2
AsociTon <asociton@outlook.com>
2019-09-14 01:59:49 +0530

    Remove unused google api and social media handles.

--- public/js/main.js
+++ public/js/main.js
@@ -67,18 +67,6 @@ document.getElementById("nav").onclick = function(){
     }
 }
 
-// Google Maps
-function initMap() {
-  // The location of Uluru
-  var uluru = {lat: -25.344, lng: 131.036};
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
-}
-
-
 // Register
 document.getElementById("internal-button").onclick = function() {
     window.location = "https://docs.google.com/forms/d/e/1FAIpQLScubhnL--kXS6vKb9zuqXdujltI-GN-k1G1sxrAjytYrYv19Q/viewform";
